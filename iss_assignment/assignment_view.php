748819b95e8a498af8a7768c2af08a33f3b2b1ff
azra10 <azra10>
2018-08-19 01:11:27 -0700

    text entered in display page for attendance and participation

--- iss_assignment/assignment_view.php
+++ iss_assignment/assignment_view.php
@@ -48,12 +48,38 @@ if (null != $post) {
         title="Printer Friendly and PDF"><img style="border:none;-webkit-box-shadow:none;box-shadow:none;" 
         src="//cdn.printfriendly.com/buttons/printfriendly-pdf-button-nobg-md.png" alt="Print Friendly and PDF"/></a>
     
-        <?php
-        echo "</div></div>";
+    </div>
+    </div>
 
-        echo "<hr/><div class ='row'><div class='col-md-12'>";
-        $shortcodetext = '[post-content id=' . $postid . ']';
-        echo do_shortcode($shortcodetext);
+    <hr/>
+    <div class ='row'>
+    <div class='col-md-12'>
+    <?php
+
+    if (strncmp($post->Name, 'participation', strlen('participation')) === 0) { ?>
+            <h3>Class Presence and Participation</h3> 
+            <span>Class presence and participation points are given to encourage active class participation and discussion.</span>
+        <?php 
+    } else if (strncmp($post->Name, 'attendance', strlen('attendance')) === 0) {
+        ?>
+            <h3>Islamic School of Silicon Valley Guidelines and Regulations</h3>
+            <h4>F. Attendance</h4>
+            <span>Attendance Regular attendance is very important as it enables students to fully benefit from the Islamic School. Attending school every week ensures that each student keeps up with the teacher’s lesson plan and assignment schedule. It also instills the sense that Islamic education is at least as important as the other activities in his/her life, and therefore, should be taken seriously.
+            </span>
+            <ul>
+                 <li>Student attendance is taken each Sunday in every grade for both the Quranic Studies and Islamic Studies class sessions.</li>
+                 <li>For Leave of Absence, both the Quranic &amp; Islamic studies teachers should be notified. All the missed activities (for example homework, notes, etc) should be made up before returning to the class.</li>
+                 <li>The attendance record of each student will be included on his/her final Report Card.</li>
+                 <li>The names and telephone numbers of a student’s teacher will be made available to parents so they can provide proper notification about necessary absences.</li>
+            </ul>
+            <strong>Enforcement</strong>
+            <span style='text-decoration: underline;'>3 Consecutive Un-Excused Absences or a large number of non-contiguous absences during the school year, could result in the student’s Suspension or Expulsion from the school.</span>           
+            <?php
+
+        } else {
+            $shortcodetext = '[post-content id=' . $postid . ']';
+            echo do_shortcode($shortcodetext);
+        }
         echo "</div></div>";
     }
     ?> 
